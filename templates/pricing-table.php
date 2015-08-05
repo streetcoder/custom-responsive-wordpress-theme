e1ce3328a3ce534c5685930ee64b192595f499aa
streetcoder <proshimul@yahoo.com>
2015-08-05 08:19:28 +0600

    added pricing table custom

---
+++ templates/pricing-table.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: streetcoder
+ * Date: 8/5/15
+ * Time: 7:44 AM
+ */
+?>
+
+
+<div class="pricing-table" id="get-started">
+    <h2 class="big-text">Our Packages</h2>
+    <div class="row">
+        <div class="col-md-4 col-sm-4">
+
+            <div class="pr-box">
+                <p class="pt-heading">Starter</p>
+
+                <div class="pr-price">
+                    <sup><i class="fa fa-usd"></i></sup>359<span class="pr-price-type">/ one-time</span>
+                </div>
+
+                <ul class="pr-feature">
+                    <li><i class="fa fa-clock-o"></i> 3 month campaign</li>
+                    <li><i class="fa fa-envelope"></i> Estimated 2,000+ customer emails</li>
+                    <li><i class="fa fa-desktop"></i> Custom designed landing page</li>
+                    <li><i class="fa fa-pencil-square-o"></i> 1 professionally copy written emails</li>
+                </ul>
+                <a href="#" type="button" class="btn btn-pricing btn-block btn-responsive">Get Started!</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-sm-4">
+            <div class="pr-box-featured">
+                <p class="pt-heading featured">Guru</p>
+                <div class="pr-price featured">
+                    <sup><i class="fa fa-usd"></i></sup>689<span class="pr-price-type">/ one-time</span>
+                </div>
+
+                <ul class="pr-feature">
+                    <li><i class="fa fa-clock-o"></i> 6 month campaign</li>
+                    <li><i class="fa fa-envelope"></i> Estimated 7,000+ customer emails</li>
+                    <li><i class="fa fa-desktop"></i> Custom designed landing page</li>
+                    <li><i class="fa fa-pencil-square-o"></i> 3 professionally copy written emails</li>
+                </ul>
+                <a href="#" type="button" class="btn btn-pricing-featured btn-block btn-responsive">Get Started!</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-sm-4">
+            <div class="pr-box">
+                <p class="pt-heading">Ultimate</p>
+                <div class="pr-price">
+                    <sup><i class="fa fa-usd"></i></sup>979<span class="pr-price-type">/ one-time</span>
+                </div>
+
+                <ul class="pr-feature">
+                    <li><i class="fa fa-clock-o"></i> 12 month campaign</li>
+                    <li><i class="fa fa-envelope"></i> Estimated 12,000+ customer emails</li>
+                    <li><i class="fa fa-desktop"></i> Custom designed landing page</li>
+                    <li><i class="fa fa-pencil-square-o"></i> 7 professionally copy written emails</li>
+                </ul>
+                <a href="#" type="button" class="btn btn-pricing btn-block btn-responsive">Get Started!</a>
+            </div>
+        </div>
+    </div>
+</div>
