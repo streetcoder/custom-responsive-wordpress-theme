75268a21bf24a612518d4c1121aed56d4fcfceaa
streetcoder <proshimul@yahoo.com>
2015-08-06 14:27:35 +0600

    added new footer, added product details , enable navigation access from other page

---
+++ templates/footer-2.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: streetcoder
+ * Date: 8/6/15
+ * Time: 11:17 AM
+ */
+?>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-3 col-sm-offset-2 col-sm-3 col-xs-offset-0 col-xs-4">
+                <img src="<?= get_template_directory_uri(); ?>/dist/images/guru-transparent.png" class="img-responsive" alt="list guru">
+            </div>
+            <div class="col-md-4 col-sm-7 col-xs-8">
+                <div class="footer-right">
+                    <h1 class="footer-brand">LIST GURU</h1>
+                    <p class="footer-brand-nav"><a href="#">Terms &amp; Conditions</a> - <a href="<?php bloginfo('url') ?>#getstarted">Get Started</a>  - <a href="<?php bloginfo('url') ?>#contact">Contact</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
